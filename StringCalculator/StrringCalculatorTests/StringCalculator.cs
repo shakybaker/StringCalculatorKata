5da8c0efae36308b1b944cb1abb84016c1f25ddc
Mark Baker <shakybaker@gmail.com>
2013-11-12 08:53:12 +0000

    forgot to add this lot

---
+++ StringCalculator/StrringCalculatorTests/StringCalculator.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace StrringCalculatorTests
+{
+    public class StringCalculator : IStringCalculator
+    {
+        public int Calculate(string input)
+        {
+            var delimiters = new string[] { ",", "\n" };
+            string args;
+
+            var delimMatch = Regex.Match(input, @"^//(?<delim>.*?)\n(?<args>.*$?)");
+            if (delimMatch.Success)
+            {
+                string delims = delimMatch.Groups["delim"].Value;
+                var argMatch = Regex.Match(delims, @"\[.*\].*$");
+                var i = 0;
+                while (argMatch.Success)
+                {
+                    Array.Resize(ref delimiters, delimiters.Length + 1);
+                    delimiters[delimiters.Length - 1] = argMatch.Groups[i].Value;
+                    argMatch.NextMatch();
+                    i++;
+                }
+
+                args = delimMatch.Groups["args"].Value;
+            }
+            
+            
+            var numArray = input.Split(delimiters, StringSplitOptions.None);
+
+            if (numArray.Length == 1)
+                return ConvertToNumber(numArray[0]);
+
+            var sum = 0;
+            for (var i = 0; i < numArray.Length; i++)
+                sum = sum + ConvertToNumber(numArray[i]);
+
+            return sum;
+        }
+
+        private static int ConvertToNumber(string input)
+        {
+            int output;
+            bool isNumber = int.TryParse(input, out output);
+
+            if (isNumber)//TODO: should throw argument for non-numeric also
+            {
+                if (output < 0)
+                    throw new ArgumentException();
+                if (output > 1000)
+                    output = 0;
+            }
+
+            return output;
+        }
+    }
+}
---
+++ StringCalculator/StrringCalculatorTests/UnitTests.cs
@@ -0,0 +1,99 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StrringCalculatorTests;
+
+namespace StringCalculatorTests
+{
+    [TestFixture]
+    public class UnitTests
+    {
+
+        [Test]
+        public void An_empty_string_returns_zero()
+        {
+            var calc = new StringCalculator();
+            var actual = calc.Calculate(string.Empty);
+
+            Assert.AreEqual(0, actual);
+        }
+
+        [Test]
+        public void A_single_number_returns_the_value()
+        {
+            var calc = new StringCalculator();
+            var actual = calc.Calculate("1");
+
+            Assert.AreEqual(1, actual);
+        }
+
+        [Test]
+        public void Two_numbers_comma_delimited_returns_the_sum()
+        {
+            var calc = new StringCalculator();
+            var actual = calc.Calculate("1,2");
+
+            Assert.AreEqual(3, actual);
+        }
+
+        [Test]
+        public void Two_numbers_newline_delimited_returns_the_sum()
+        {
+            var calc = new StringCalculator();
+            var actual = calc.Calculate("1\n2");
+
+            Assert.AreEqual(3, actual);
+        }
+
+        [Test]
+        public void Three_numbers_delimited_either_way_returns_the_sum()
+        {
+            var calc = new StringCalculator();
+            var actual = calc.Calculate("1\n2,3");
+
+            Assert.AreEqual(6, actual);
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentException))]
+        public void Negative_numbers_throw_an_exception()
+        {
+            var calc = new StringCalculator();
+            var actual = calc.Calculate("-1");
+        }
+
+        [Test]
+        public void Numbers_greater_than_1000_are_ignored()
+        {
+            var calc = new StringCalculator();
+            var actual = calc.Calculate("1,1001");
+
+            Assert.AreEqual(1, actual);
+        }
+
+        [Test]
+        public void A_single_char_delimiter_can_be_defined_on_the_first_line()
+        {
+            //NOTE: my assumption here is that there will be a '\n' after the delimiter
+            var calc = new StringCalculator();
+            var actual = calc.Calculate("//[#][x][y][xxx]\n1#2");
+
+            Assert.AreEqual(3, actual);
+        }
+
+        /*
+        [Test]
+        public void A_multi_char_delimiter_can_be_defined_on_the_first_line()
+        {
+        }
+
+        [Test]
+        public void Many_single_or_multichar_delimiters_can_be_defined()
+        {
+        }
+         */
+    }
+}
